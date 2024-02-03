498654a6c4036d8084cb0b8bb865263e2eee4683
V.I.P <sochoeun162@gmail.com>
2024-02-03 19:44:33 +0700

    use local images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-
+import img from "../assets/images/profile.jpg";
 export default function Image() {
-  return (
-    <img
-      src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=600"
-      alt="profile"
-    />
-  );
+  return <img src={img} alt="profile" />;
 }
