b66429916f306f444b177c61375a0b4162a4343c
V.I.P <sochoeun162@gmail.com>
2024-02-06 19:05:17 +0700

    filter shoe

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -12,33 +12,35 @@ function Product() {
     },
     {
       id: 2,
-      img: "https://i.pinimg.com/564x/00/db/fd/00dbfd3737555e058ee3c2d02bdfed24.jpg",
-      brand: "Addidas",
+      img: "https://i.pinimg.com/564x/1f/9d/ca/1f9dcac4e16c4b2582b43f70cfb0f630.jpg",
+      brand: "Nike",
       price: "$ 250.00",
       detial: "No comments yet! Add one to start the conversation.",
     },
     {
       id: 3,
-      img: "https://i.pinimg.com/564x/00/db/fd/00dbfd3737555e058ee3c2d02bdfed24.jpg",
+      img: "https://i.pinimg.com/564x/10/42/0c/10420c006334ab25ac01e6fd038c2c46.jpg  ",
       brand: "Addidas",
       price: "$ 250.00",
       detial: "No comments yet! Add one to start the conversation.",
     },
     {
       id: 4,
-      img: "https://i.pinimg.com/564x/00/db/fd/00dbfd3737555e058ee3c2d02bdfed24.jpg",
-      brand: "Addidas",
+      img: "https://i.pinimg.com/564x/42/54/cf/4254cf545f22b6525d335ff11e45a723.jpg",
+      brand: "Nike",
       price: "$ 250.00",
       detial: "No comments yet! Add one to start the conversation.",
     },
   ];
+  const filterShoes = shoe.filter((fs) => {
+    return fs.brand === "Nike";
+  });
   return (
     <React.Fragment>
-      {shoe.map((n) => {
-        const { img, brand, price, detail } = n;
+      {filterShoes.map(({ id, img, brand, price, detail }) => {
         return (
           <Card
-            key={n.id}
+            key={id}
             img={img}
             brand={brand}
             price={price}
